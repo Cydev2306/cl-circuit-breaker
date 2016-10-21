48213a6f94e01abada0065b8ef20e077fb646b38
Luc Chmielowski <luc.chmielowski@gmail.com>
2016-10-21 14:05:08 +0200

    Fixed timeout

--- app.js
+++ app.js
@@ -10,35 +10,38 @@ var librato = require('librato-node');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 
+app.use(timeout('2s', { respond: true }));
+
 librato.configure({email: process.env.LIBRATO_MAIL, token: process.env.LIBRATO_TOKEN});
 librato.start()
 app.use(bodyParser.json()); // for parsing application/json
+app.use(haltOnTimeout);
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(haltOnTimeout);
 app.use(librato.middleware());
-
+app.use(haltOnTimeout);
 // Custom middlewares
 var breaker = require('./middlewares/breaker');
-
 app.use(morgan('dev'));
+app.use(haltOnTimeout);
 app.use(cors());
-app.use(timeout('2s', { respond: false }));
-app.use(breaker(2, librato));
+app.use(haltOnTimeout);
+app.use(breaker(5, librato));
+app.use(haltOnTimeout);
 
-app.use((req, res, next) => {
-  if(!req.timedout || req.circuitStatus === 'OPEN') {
-    next();
-  } else {
-    res.status(503).json({error:  'Timeout error.'});
+function haltOnTimeout(req, res, next) {
+  if(!req.timedout) {
+    return next();
   }
-});
+}
 
 app.post('/orders', function(req ,res) {
   if(req.circuitStatus === 'CLOSE') {
     librato.measure('orders-create', 1);
-    return callApi(`orders`, 'post', req, res, false);
+    callApi(`orders`, 'post', req, res, false);
+  }else{
+    res.status(403).json({error: "Can't create order. API unavailable."});
   }
-
-  return res.status(403).json({error: "Can't create order. API unavailable."});
 });
 
 app.get('/orders/:id', function(req, res) {
